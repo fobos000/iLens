86e35c68b2ebea97d2372944d01a8d0c53a2ce29
Ostap Horbach <phnx@ostaps-mbp-4.dlink>
2017-10-14 13:06:58 +0300

    Initial Commit

---
+++ iLens/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iLens
+//
+//  Created by Ostap Horbach on 10/14/17.
+//  Copyright © 2017 Ostap Horbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
